9e31ae96ac2978ba3cfdffaa1809415378dec183
Neuza Sousa <neuzarssousa@gmail.com>
2021-02-22 15:53:31 +0100

    Still working in rendering array

--- src/App.js
+++ src/App.js
@@ -25,6 +25,37 @@ class App extends React.Component {
 
   
 
+  async componentDidMount() {
+    let error = '';
+    try {
+      const { data:root } = await octokit.request("GET /search/repositories", {
+        q: `stars:>=10 created:>2021-02-07 language:python`
+      });
+
+      if({data:root}.data["items"]) {
+        // server acknowledged request: no data received yet
+        repos = await {data:root}.data["items"]; // wait for your data to arrive
+      } else {
+        // server contacted; error occured
+        //error = `ERROR: ${response.status} ${response.statusText}`;
+      }
+    } catch (err) {
+    // server not contacted
+    error = `EXCEPTION: ${err.message}`
+    }
+    
+    this.setState({
+      repos: repos,
+      name1: repos[0]["full_name"],
+      name2: repos[1]["full_name"],
+      name3: repos[2]["full_name"],
+    })
+    //this.getNames();
+    console.log(repos);
+    console.log(this.state.repos)
+    //console.log(this.state.names);
+  }
+
   async getResults() {
     let error = '';
     try {
@@ -50,13 +81,15 @@ class App extends React.Component {
       name2: repos[1]["full_name"],
       name3: repos[2]["full_name"],
     })
-    this.getNames();
-    //console.log(repos);
-    //console.log(this.state.repos)
-    //console.log(this.state.names);
+    //this.getNames();
+    console.log(repos);
+    console.log(this.state.repos)
+    console.log(this.state.repos[0].full_name);
+    console.log(this.state.repos[0].id);
   }
 
-  getNames() {
+
+  /*getNames() {
     let full_name = [];
     for(let repo of this.state.repos){
         full_name.push(repo.full_name);
@@ -68,7 +101,7 @@ class App extends React.Component {
     console.log(this.state.names);
     //console.log(this.state.organizedRepos);
     }
-  }
+  }*/
 
   handleChange (event) {
     this.setState({ language: event.target.value });
@@ -86,8 +119,8 @@ class App extends React.Component {
 
   render(){
     let itemsJsx = this.state.repos.map(repo => {
-      return <li key={repo.id}>{repo.name}</li>
-   })
+      return <li key={repo.id}>{repo.full_name}</li>
+    })
     //let namesJsx = "hi" //this.state.names.map(name => (
       //<li>
         //{name}
@@ -113,9 +146,9 @@ class App extends React.Component {
       <h3>{this.state.name1}</h3>
       <h3>{this.state.name2}</h3>
       <h3>{this.state.name3}</h3>
-      {this.state.names
+      {this.state.repos}
       ? <ul>{itemsJsx}</ul>
-      : <p>Hello</p>}
+    : <p>Hello</p>
     </div>
     )
   }
